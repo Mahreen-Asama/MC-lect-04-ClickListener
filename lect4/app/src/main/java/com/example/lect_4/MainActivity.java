a3d69a90f2f9a46d57c2a48a58fff0509887fe91
Mahreen Asama <79749919+Mahreen-Asama@users.noreply.github.com>
2022-04-27 20:28:55 +0500

    Update MainActivity.java

--- lect4/app/src/main/java/com/example/lect_4/MainActivity.java
+++ lect4/app/src/main/java/com/example/lect_4/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.lect_4;
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
