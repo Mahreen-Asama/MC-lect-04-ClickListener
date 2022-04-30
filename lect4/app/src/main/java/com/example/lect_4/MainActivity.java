e54a2168563895c3b004e2d596893527fab9e36c
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-04-30 14:23:53 +0500

    btn listener added

--- lect4/app/src/main/java/com/example/lect_4/MainActivity.java
+++ lect4/app/src/main/java/com/example/lect_4/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.lect_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b,b2;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int v=12;
+        b2.setId(v);
+        b2.setText("aoa");
+        b2.setWidth(30);
+        b2.setHeight(50);
+
+        b=findViewById(R.id.button4);
+        tv=findViewById(R.id.textView);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(R.string.hello);
+            }
+        });
+    }
+
+    public void writeText(View view) {
+        tv.setText("aoa");
     }
 }
